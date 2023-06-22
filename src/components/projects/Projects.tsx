5a9c61580c33f41ebbee14fe06e6847a5982c0dd
dranyam2022 <maynardescalante@gmail.com>
2023-06-22 13:49:15 +0800

    done with projects placeholder

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,8 @@
+import styles from "./Projects.module.css";
+export const Projects = () => {
+  return (
+    <section className={styles.container}>
+      <h2>New Projects Comming Soon...</h2>
+    </section>
+  );
+};
