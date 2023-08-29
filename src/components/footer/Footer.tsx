dc161f129a0baaeef2ffd32db7f6104b63b1273f
dranyam2022 <maynardescalante@gmail.com>
2023-08-29 10:57:11 +0800

    corrected text and styles for copywrite

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,9 +2,16 @@ import styles from "./Footer.module.css";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Footer = () => {
+  const copywrite = `© 2023 MAYNARD ESCALANTE. All rights reserved.
+Unauthorized use of any materials on this site is prohibited.
+For permissions, contact maynardescalante@gmail.com.
+`;
+
   return (
     <footer className={styles.container}>
-      <h3>Copyright @ 2023. All rights resserved</h3>
+      <h3 className={styles.copywrite}>
+        {typeof copywrite === "string" ? copywrite : ""}
+      </h3>
       <div className={styles.socials}>
         <a
           href="https://www.linkedin.com/in/maynard-escalante-3033ab1a3/"
